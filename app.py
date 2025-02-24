9747d626374328c848b2b19c3b1d710035572145
Yan <ydnlv@vk.com>
2025-02-24 17:34:42 +0900

    2402v6

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-   message = 'Добро пожаловать!'
+   message = 'Добро пожаловать! v2'
    return render_template('index.html', message=message)
